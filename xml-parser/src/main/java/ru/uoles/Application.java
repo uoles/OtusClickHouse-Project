98ad5008b82c022d08daa7f3b668de192ccb84d9
uoles <max.uoles@rambler.ru>
2025-04-20 04:45:01 +0300

    Add files.

---
+++ xml-parser/src/main/java/ru/uoles/Application.java
@@ -0,0 +1,15 @@
+package ru.uoles;
+
+/**
+ * xml-parser
+ * Created by Intellij IDEA.
+ * Developer: uoles (Kulikov Maksim)
+ * Date: 20.04.2025
+ * Time: 4:30
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
