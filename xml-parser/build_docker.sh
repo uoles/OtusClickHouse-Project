7db03b56dc15fe4e06348d86a506dfa5571f740f
uoles <max.uoles@rambler.ru>
2025-05-18 21:01:38 +0300

    Refactoring.

--- xml-parser/build_docker.sh
+++ xml-parser/build_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker build -f app.Dockerfile -t uoles/xml-parser:1.0.1 .
 
+docker rm xml-parser
 docker run -d --name xml-parser \
 	  --publish 8090:8090 \
     uoles/xml-parser:1.0.1
