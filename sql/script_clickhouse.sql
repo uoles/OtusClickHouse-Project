e951752995445817818a4eb9b95ec62929c1a648
uoles <max.uoles@rambler.ru>
2025-05-18 22:11:19 +0300

    Add clickhouse dockerfile.

---
+++ sql/script_clickhouse.sql
@@ -0,0 +1,46 @@
+CREATE TABLE valute_data
+(
+    c_date String,
+    c_name String,
+    c_str_id String,
+    c_num_code String,
+    c_char_code String,
+    c_nominal String,
+    c_value String
+)
+ENGINE = MergeTree 
+ORDER BY (c_date, c_str_id);
+
+CREATE TABLE valute_data_queue
+(
+    c_date String,
+    c_name String,
+    c_str_id String,
+    c_num_code String,
+    c_char_code String,
+    c_nominal String,
+    c_value String
+)
+ENGINE = Kafka
+SETTINGS 
+    kafka_broker_list = 'kafka:9092', 
+    kafka_topic_list = 'valute_topic',
+    kafka_group_name = 'valute_group',
+    kafka_format = 'CSV', 
+    kafka_num_consumers = 1, 
+    kafka_skip_broken_messages = 10,
+    kafka_thread_per_consumer = 0;
+
+CREATE MATERIALIZED VIEW valute_data_queue_mv TO valute_data
+( 
+    c_date String,
+    c_name String,
+    c_str_id String,
+    c_num_code String,
+    c_char_code String,
+    c_nominal String,
+    c_value String
+) AS SELECT message FROM valute_data_queue;
+
+select *
+from valute_data_queue_mv
