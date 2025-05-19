a4770abc75c46e6747fe788e26370ddcef06e68f
uoles <max.uoles@rambler.ru>
2025-05-20 01:30:26 +0300

    Add postgres docker build script.

---
+++ docker/build_docker_postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker stop postgresql
+docker rm postgresql
+
+docker run --name postgresql \
+    -e POSTGRES_PASSWORD=passwordpg1234 \
+    -e POSTGRES_USER=userpg \
+    -e POSTGRES_DB=pgdb \
+    -p 5433:5432 \
+    -v "./postgresql/data/":"/var/lib/postgresql/data/" \
+    -d postgres:latest
