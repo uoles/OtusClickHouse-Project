c6de1ae31a46ef459fbd3996c41774259d8178b9
uoles <max.uoles@rambler.ru>
2025-05-19 20:38:18 +0300

    Refactoring.

--- sql/script_clickhouse.sql
+++ sql/script_clickhouse.sql
@@ -1,51 +1,70 @@
 
 CREATE TABLE valute_data
 (
-    c_date String,
-    c_name String,
-    c_str_id String,
-    c_num_code String,
-    c_char_code String,
-    c_nominal String,
-    c_value String
+	id UInt64,
+    date Date,
+    name String,
+    str_id String,
+    num_code String,
+    char_code String,
+    nominal String,
+    value String
 )
-ENGINE = MergeTree 
-ORDER BY (c_date, c_str_id);
+ENGINE = MergeTree
+ORDER BY (id, date, str_id);
+
+DROP TABLE valute_data;
 
 CREATE TABLE valute_data_queue
 (
-    c_date String,
-    c_name String,
-    c_str_id String,
-    c_num_code String,
-    c_char_code String,
-    c_nominal String,
-    c_value String
+	id UInt64,
+    date String,
+    name String,
+    str_id String,
+    num_code String,
+    char_code String,
+    nominal String,
+    value String
 )
 ENGINE = Kafka
-SETTINGS 
-    kafka_broker_list = 'kafka:9092', 
+SETTINGS
+    kafka_broker_list = 'kafka:9092',
     kafka_topic_list = 'valute_topic',
     kafka_group_name = 'valute_group',
-    kafka_format = 'CSV', 
-    kafka_num_consumers = 1, 
+    kafka_format = 'JSONEachRow',
+  	kafka_row_delimiter = '\n',
+    kafka_num_consumers = 1,
     kafka_skip_broken_messages = 10,
     kafka_thread_per_consumer = 0;
 
+DROP TABLE valute_data_queue;
+
 CREATE MATERIALIZED VIEW valute_data_queue_mv TO valute_data
-( 
-    c_date String,
-    c_name String,
-    c_str_id String,
-    c_num_code String,
-    c_char_code String,
-    c_nominal String,
-    c_value String
-) AS SELECT c_date, c_name, c_str_id, c_num_code, c_char_code, c_nominal, c_value  
+(
+	id UInt64,
+    date Date,
+    name String,
+    str_id String,
+    num_code String,
+    char_code String,
+    nominal String,
+    value String
+) AS
+SELECT id, parseDateTimeBestEffortOrNull(date,'Europe/Moscow') AS date, name, str_id, num_code, char_code, nominal, value
 FROM valute_data_queue;
 
+DROP TABLE valute_data_queue_mv;
 
 select *
-from valute_data_queue_mv;
+from valute_data_queue_mv
+order by date desc;
 
+
+select count(1)
+from valute_data_queue_mv
+where date = toDate('2000-03-19');
+
+SELECT toDate('2000-03-19') AS date;
+
+SELECT parseDateTimeBestEffortOrNull('19/05/2002','Europe/Moscow') AS parsed_date;
 
--- sql/script_pg.sql
+++ sql/script_pg.sql
@@ -26,24 +26,70 @@ from pgclick.valute_data
 where str_id = 'R01770'
 limit 200
 
-----
+
+select count(*)
+from pgclick.valute_data
+
+drop table pgclick.valute_data;
+
+
+select *
+from pgclick.valute_data
+where date = '19/05/2025';
+
+select *
+from pgclick.valute_data
+order by id desc
+limit 100;
+
+delete from pgclick.valute_data
+where id > 200000;
+
+select count(1)
+from pgclick.valute_data
+where date = '19/05/2003';
+
+
+INSERT INTO pgclick.valute_data( id, date, name, str_id, num_code, char_code, nominal, value )
+    VALUES( nextval('pgclick.seq_valute_id'), '19/05/2009', 'Азербайджанский манат', 'R01020A', '944', 'AZN', '1', '47,5111' )
+    ON CONFLICT ( date, str_id ) DO nothing;
+
+INSERT INTO pgclick.valute_data( id, date, name, str_id, num_code, char_code, nominal, value )
+    VALUES( nextval('pgclick.seq_valute_id'), '19/03/2000', 'Иен', 'R01820', '392', 'JPY', '100', '55,6950' )
+    ON CONFLICT ( date, str_id ) DO nothing;
+
+delete from pgclick.valute_data
+where date = '19/05/2009';
+
 
 ALTER SYSTEM SET wal_level = logical;
 
+
 SHOW wal_level;
 
-CREATE PUBLICATION pgclick_publication FOR TABLES IN SCHEMA pgclick;
 
-CREATE TABLE IF NOT EXISTS pgclick.debezium_offset_storage(
-    id VARCHAR(36) NOT NULL,
+CREATE PUBLICATION pgclick_publication
+	FOR TABLES IN SCHEMA pgclick
+	with(publish = 'insert');
+
+
+drop PUBLICATION pgclick_publication;
+
+
+
+CREATE TABLE IF NOT EXISTS pgclick.debezium_offset_storage(id VARCHAR(36) NOT NULL,
     offset_key VARCHAR(1255),
     offset_val VARCHAR(1255),
     record_insert_ts TIMESTAMP NOT NULL,
     record_insert_seq INTEGER NOT NULL
 );
 
+
 select *
 from pgclick.debezium_offset_storage;
+
+drop table pgclick.debezium_offset_storage;
+
 
 SELECT
    slot_name,
@@ -51,4 +97,8 @@ SELECT
    pg_size_pretty(pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)) as slot_lag,
    pg_size_pretty(pg_wal_lsn_diff(pg_current_wal_lsn(), confirmed_flush_lsn)) as confirmed_lag
 FROM pg_replication_slots
-WHERE slot_name = 'pgclick_slot';
+WHERE slot_name = 'pgclick_slot'
+
+SELECT pg_drop_replication_slot('pgclick_slot');
+
+
